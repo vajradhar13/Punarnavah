4a8ab6c1ce79dba821ef73b49057cd039443b55f
vajradhar13 <dvajradhar@gmail.com>
2026-01-01 19:45:28 +0530

    updated the code

--- server/src/index.ts
+++ server/src/index.ts
@@ -48,5 +48,5 @@ app.get("/health", (req, res) => {
   res.status(200).json({ status: "ok" });
 });
 app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
+  console.log(`Server is running on port ${process.env.PORT} `);
 });
